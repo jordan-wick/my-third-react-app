cd2fdb6c1dd7b5acda86e463cf48f89e44bbb5b4
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-10-22 22:59:55 -0400

    More sandboxing

---
+++ src/Foodie.jsx
@@ -0,0 +1,8 @@
+function Foodie() {
+  return <section>
+    <p>Nothing beats a great steak...</p>
+    <p>Especially paired with some excellent light risotto.</p>
+    </section>;
+}
+
+export default Foodie;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Greeting from './Greeting.jsx'
+import Foodie from './Foodie.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting />
+    <Foodie />
   </StrictMode>,
 )
